2a1fe25574de573445ae3a0c0d1e4c1f6b7854ea
Nathanael Björkgård <nathanael@bjorkgard.se>
2024-06-01 12:17:36 +0200

    Fix

--- app/Http/Controllers/ServiceGroupReportController.php
+++ app/Http/Controllers/ServiceGroupReportController.php
@@ -14,9 +14,8 @@ class ServiceGroupReportController extends Controller
         app()->setLocale($locale);
 
         $serviceGroup = ServiceGroup::where('service_group_identifier', $identifier)->with('reports')->firstOrFail();
-        Log::info('Identifier',$serviceGroup->identifier);
         $congregation = Congregation::where('identifier', $serviceGroup->identifier)->firstOrFail();
-        Log::info($congregation);
+
 
         return Inertia::render('Reports/ServiceGroup', [
             'congregation' => $congregation,
